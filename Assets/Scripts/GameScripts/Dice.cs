06eff497d2588667fc11dd7f77dc79a7a61d5f75
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2022-01-19 23:14:52 +0530

    changes updated

--- Assets/Scripts/GameScripts/Dice.cs
+++ Assets/Scripts/GameScripts/Dice.cs
@@ -7,6 +7,7 @@ public class Dice : MonoBehaviour {
     
     [SerializeField] public Sprite[] diceFaces;
     [SerializeField] public SpriteRenderer dice;
+    [SerializeField] public BoxCollider2D diceCollider;
 
     public string diceID;
 
@@ -27,8 +28,10 @@ public class Dice : MonoBehaviour {
 
     private void ShowOrHideDice(PlayerTurnSignal signal) {
         if(signal.color.ToLower().Equals(diceID.ToLower())) {
+            diceCollider.enabled = true;
             gameObject.SetActive(true);
         } else {
+            diceCollider.enabled = false;
             gameObject.SetActive(false);
         }
     }
@@ -43,14 +46,23 @@ public class Dice : MonoBehaviour {
             RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
             if (hit.collider != null && hit.transform.GetComponent<Dice>() != null) {
                 if (hit.transform.GetComponent<Dice>().diceID.Equals(diceID)) {
-                    int roll = Random.Range(0, 6);
-                    dice.sprite = diceFaces[roll];
-                    _signalBus.Fire(new DiceResultSignal { 
-                        roll = roll + 1,
-                        color = diceID
-                    });
+                    StartCoroutine(RollDice());
                 }
             }
         }
+    }
+
+    private IEnumerator RollDice() {
+        for(int i = 0;i < 10;i++) {
+            dice.sprite = diceFaces[Random.Range(0, 6)];
+            yield return new WaitForSeconds(0.1f);
+        }
+        int roll = Random.Range(0, 6);
+        dice.sprite = diceFaces[roll];
+        _signalBus.Fire(new DiceResultSignal {
+            roll = roll + 1,
+            color = diceID
+        });
+        diceCollider.enabled = false;
     }
 }
