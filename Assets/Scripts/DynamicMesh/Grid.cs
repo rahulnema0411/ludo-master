ff994c52ee01ed012e82054d21959a396c44059c
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2023-02-23 08:03:08 +0530

    code refactor grid script

--- Assets/Scripts/DynamicMesh/Grid.cs
+++ Assets/Scripts/DynamicMesh/Grid.cs
@@ -1,30 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
-using Zenject;
 
-public class Grid {
+public class CustomGrid<T> {
     private int columns;
     private int rows;
     private int[,] gridArray;
     private Transform parentTransform;
     private float cellSizeX, cellSizeY;
     private float gridSize;
-    private BoxCollider2D[,] boxColliderArray;
+    private T[,] cellArray;
     private List<Vector2> greenCells;
-
-    private SignalBus _signalBus;
-    private LudoBoard _ludoBoard;
-
-    [Inject]
-    public void Construct(SignalBus signalBus, LudoBoard ludoBoard) {
-        _signalBus = signalBus;
-        _ludoBoard = ludoBoard;
-    }
 
     public float CellSizeX { get => cellSizeX; set => cellSizeX = value; }
     public float CellSizeY { get => cellSizeY; set => cellSizeY = value; }
+    public T[,] CellArray { get => cellArray; set => cellArray = value; }
 
-    public Grid(int columns, int rows, float gridSize, Transform parentTransform) {
+    public CustomGrid(int columns, int rows, float gridSize, Transform parentTransform) {
         this.rows = rows;
         this.columns = columns;
         this.parentTransform = parentTransform;
@@ -33,55 +24,39 @@ public class Grid {
         CellSizeX = gridSize/ rows;
         CellSizeY = gridSize/ columns;
         gridArray = new int[columns, rows];
-        boxColliderArray = new BoxCollider2D[columns, rows];
+        cellArray = new T[columns, rows];
+        ConstructGrid();
     }
 
-    public void SetUpGrid() {
+    private void ConstructGrid() {
         for (int i = 0; i < gridArray.GetLength(0); i++) {
             for (int j = 0; j < gridArray.GetLength(1); j++) {
-                boxColliderArray[i, j] = CreateColliderMesh(parentTransform, i, j, GetWorldPosition(i, j) + new Vector3(CellSizeX - gridSize, CellSizeY - gridSize) * 0.5f);
+                CellArray[i, j] = CreateGridCell(parentTransform, i, j, GetWorldPosition(i, j) + new Vector3(CellSizeX - gridSize, CellSizeY - gridSize) * 0.5f);
             }
         }
     }
 
-    public Vector3 GetWorldPosition(int x, int y) {
+    private Vector3 GetWorldPosition(int x, int y) {
         return new Vector3(x * CellSizeX, y * CellSizeY);
     }
 
-
-    public BoxCollider2D CreateColliderMesh(Transform parent = null, int i = 0, int j = 0, Vector3 localPosition = default, Color? color = null, int sortingOrder = 5000) {
-        GameObject gameObject = new GameObject("Cell", typeof(BoxCollider2D));
+    private T CreateGridCell(Transform parent = null, int i = 0, int j = 0, Vector3 localPosition = default, Color? color = null) {
+        GameObject gameObject = new GameObject("Cell", typeof(T));
         gameObject.AddComponent(typeof(MeshFilter));
         gameObject.AddComponent(typeof(MeshRenderer));
+
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        meshFilter.mesh = ImageHelper.instance.getQuadMesh();
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        renderer.material = ImageHelper.instance.GetBlackMaterial();
+
         Transform transform = gameObject.transform;
         transform.SetParent(parent, false);
         transform.localPosition = localPosition;
         transform.localScale = new Vector2(CellSizeX, CellSizeY);
-        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-        meshFilter.mesh = ImageHelper.instance.getQuadMesh();
-        BoxCollider2D boxCollider = gameObject.GetComponent<BoxCollider2D>();
-        boxCollider.isTrigger = true;
-        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
-        renderer.material = ImageHelper.instance.GetBlackMaterial();
-        return boxCollider;
+
+        return gameObject.GetComponent<T>();
     }
 
-    public void HighlightPortion(int x1, int x2,  int y1, int y2) {
-        for (int i = 0; i < gridArray.GetLength(0); i++) {
-            for (int j = 0; j < gridArray.GetLength(1); j++) {
-                if((i >= x1 && i < x2) && j >= y1 && j < y2) {
-
-                    if(i == x1 || i == x2-1 || j == y1 || j == y2-1) {
-                        BoxCollider2D collider2D = boxColliderArray[i, j];
-
-                        collider2D.GetComponent<MeshRenderer>().material = ImageHelper.instance.GetWhiteMaterial();
-                        collider2D.gameObject.AddComponent(typeof(Square));
-                        collider2D.GetComponent<Square>().Construct(_signalBus, _ludoBoard);
-                        collider2D.GetComponent<Square>().SubcribeToSignals();
-                    } 
-
-                } 
-            }
-        }
-    }
+    
 }
--- Assets/Scripts/DynamicMesh/LudoGrid.cs
+++ Assets/Scripts/DynamicMesh/LudoGrid.cs
@@ -3,6 +3,8 @@ using Zenject;
 
 public class LudoGrid : MonoBehaviour {
 
+    private CustomGrid<Square> grid;
+
     private SignalBus _signalBus;
     private LudoBoard _ludoBoard;
 
@@ -14,12 +16,31 @@ public class LudoGrid : MonoBehaviour {
     
     void Start() {
 
-        Grid grid = new Grid(15, 15, 4.0f, transform);
-        grid.Construct(_signalBus, _ludoBoard);
-        grid.SetUpGrid();
-        grid.HighlightPortion(6, 9, 0, 6);
-        grid.HighlightPortion(0, 6, 6, 9);
-        grid.HighlightPortion(9, 15, 6, 9);
-        grid.HighlightPortion(6, 9, 9, 15);
+        grid = new CustomGrid<Square>(15, 15, 4.0f, transform);
+        
+        HighlightPortion(6, 9, 0, 6);
+        HighlightPortion(0, 6, 6, 9);
+        HighlightPortion(9, 15, 6, 9);
+        HighlightPortion(6, 9, 9, 15);
+        
+    }
+
+    public void HighlightPortion(int x1, int x2,  int y1, int y2) {
+        
+        for (int i = 0; i < grid.CellArray.GetLength(0); i++) {
+            for (int j = 0; j < grid.CellArray.GetLength(1); j++) {
+
+                if((i >= x1 && i < x2) && j >= y1 && j < y2) {
+
+                    if(i == x1 || i == x2-1 || j == y1 || j == y2-1) {
+                        Square cell = grid.CellArray[i, j].GetComponent<Square>();
+                        cell.Construct(_signalBus, _ludoBoard);
+                        cell.SubcribeToSignals();
+                        cell.GetComponent<MeshRenderer>().material = ImageHelper.instance.GetWhiteMaterial();
+                    } 
+
+                } 
+            }
+        }
     }
 }
